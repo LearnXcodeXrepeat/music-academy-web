165c8372ed72ce23fd02333b671414f4eac58099
viraljain7 <veerljain1234@gmail.com>
2024-06-09 13:38:34 +0530

    Added: navigation bar to improve site navigation and user experience

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-40 bg-gray-800">
+      <h1>Footer</h1>
+      End
+    </div>
+  );
+};
+
+export default Footer;
